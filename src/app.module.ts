cf97aa0c195bdf5c4a88fd6d1bd035d78a95afac
HyeongJin <wkzkfmxk23@gmail.com>
2022-01-03 23:33:19 +0900

    controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { TestController } from './test/test.controller';
+import { CatsModule } from './cats/cats.module';
+import { UserModule } from './user/user.module';
+import { User.SigninController } from './user.signin/user.signin.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [CatsModule, UserModule],
+  controllers: [AppController, CatsController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
