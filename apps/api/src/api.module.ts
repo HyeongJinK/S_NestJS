a54876be210492cbe56160c2c0ba440c5250deda
HyeongJinKim <hyeongjin.kim@oysterable.com>
2022-02-10 00:21:51 +0900

    [modify] ormModule 이름 수정

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
 import { realTypeOrmModule } from '../../../libs/model/RealTypeOrmModule';
-import { machineOrmModule } from '../../../libs/machine/RealTypeOrmModule';
+import { userOrmModule } from '../../../libs/user/UserTypeOrmModule';
 import { BannerModule } from './banner/banner.module';
 import { ConfigModule } from '@nestjs/config';
 import { GlobalExceptionFilter } from '@app/common/filter/global-exception.filter';
 import { HealthModule } from './health/health.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: [`.env`, `.env.${process.env.NODE_ENV}`],
     }),
     realTypeOrmModule(),
-    machineOrmModule(),
+    userOrmModule(),
     HealthModule,
     BannerModule,
+    UserModule,
   ],
   providers: [
     {
